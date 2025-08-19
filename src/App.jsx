956ca5de3a4cae1e92711e17bf5bec18081f2852
sourabhBisht27 <zayn9874.sb@gmail.com>
2025-08-19 15:28:33 +0530

    Implemented Art section and Hero section animation

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktail from "./components/Cocktail";
 import About from "./components/About";
+import Art from "./components/Art";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -14,6 +15,7 @@ const App = () => {
       <Hero />
       <Cocktail />
       <About />
+      <Art />
     </main>
   );
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,13 +6,20 @@ import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
   const videoRef = useRef();
+
   const isMobile = useMediaQuery({ maxWidth: 767 });
 
   useGSAP(() => {
-    const heroSplit = new SplitText(".title", { type: "chars, words" });
-    const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
+    const heroSplit = new SplitText(".title", {
+      type: "chars, words",
+    });
 
-    heroSplit.chars.forEach((char) => char.classList.add(".text-gradient"));
+    const paragraphSplit = new SplitText(".subtitle", {
+      type: "lines",
+    });
+
+    // Apply text-gradient class once before animating
+    heroSplit.chars.forEach((char) => char.classList.add("text-gradient"));
 
     gsap.from(heroSplit.chars, {
       yPercent: 100,
@@ -21,7 +28,7 @@ const Hero = () => {
       stagger: 0.06,
     });
 
-    gsap.from(paragraphSplit.chars, {
+    gsap.from(paragraphSplit.lines, {
       opacity: 0,
       yPercent: 100,
       duration: 1.8,
@@ -40,12 +47,13 @@ const Hero = () => {
         },
       })
       .to(".right-leaf", { y: 200 }, 0)
-      .to(".left-leaf", { y: -200 }, 0);
+      .to(".left-leaf", { y: -200 }, 0)
+      .to(".arrow", { y: 100 }, 0);
 
     const startValue = isMobile ? "top 50%" : "center 60%";
     const endValue = isMobile ? "120% top" : "bottom top";
 
-    const tl = gsap.timeline({
+    let tl = gsap.timeline({
       scrollTrigger: {
         trigger: "video",
         start: startValue,
@@ -69,6 +77,7 @@ const Hero = () => {
         className="noisy"
       >
         <h1 className="title">MOJITO</h1>
+
         <img
           src="/images/hero-left-leaf.png"
           alt="left-leaf"
@@ -79,21 +88,23 @@ const Hero = () => {
           alt="right-leaf"
           className="right-leaf"
         />
+
         <div className="body">
           <div className="content">
             <div className="space-y-5 hidden md:block">
               <p>Cool. Crisp. Classic.</p>
               <p className="subtitle">
-                Sip the spirit <br /> of Summer
+                Sip the Spirit <br /> of Summer
               </p>
             </div>
+
             <div className="view-cocktails">
               <p className="subtitle">
                 Every cocktail on our menu is a blend of premium ingredients,
                 creative flair, and timeless recipes — designed to delight your
-                sense
+                senses.
               </p>
-              <a href="#cocktails">View Cocktails</a>
+              <a href="#cocktails">View cocktails</a>
             </div>
           </div>
         </div>
@@ -102,11 +113,11 @@ const Hero = () => {
       <div className="video absolute inset-0">
         <video
           ref={videoRef}
-          src="/videos/output.mp4"
           muted
           playsInline
           preload="auto"
-        ></video>
+          src="/videos/output.mp4"
+        />
       </div>
     </>
   );
